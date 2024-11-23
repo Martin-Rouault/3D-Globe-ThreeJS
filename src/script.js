8213c27395ea92863916104f95c6977cbcbf7062
Martin <martin@Mac-mini-de-Martin.local>
2024-11-23 18:05:13 +0100

    globe

---
+++ src/script.js
@@ -0,0 +1,125 @@
+import * as THREE from "three";
+import { OrbitControls } from "three/examples/jsm/controls/OrbitControls.js";
+import getStarfield from "./getStarfield";
+import { drawThreeGeo } from "./threeGeoJSON";
+
+/**
+ * Base
+ */
+
+// Canvas
+const canvas = document.querySelector("canvas.webgl");
+
+// Scene
+const scene = new THREE.Scene();
+scene.fog = new THREE.FogExp2(0x000000, 0.2);
+
+/**
+ * globe
+ */
+const globe = new THREE.Group();
+scene.add(globe);
+
+const geometry = new THREE.SphereGeometry(2);
+const lineMaterial = new THREE.LineBasicMaterial({
+  color: 0xffffff,
+  transparent: true,
+  opacity: 0.3,
+});
+
+const edges = new THREE.EdgesGeometry(geometry, 1);
+const lines = new THREE.LineSegments(edges, lineMaterial);
+scene.add(lines);
+globe.add(lines)
+
+/**
+ * stars
+ */
+const stars = getStarfield({ numStars: 1000, fog: false });
+scene.add(stars);
+
+/**
+ * GeoJson
+ */
+fetch("./geoJson/ne_110m_land.json")
+  .then((response) => response.text())
+  .then((text) => {
+    const data = JSON.parse(text);
+    const countries = drawThreeGeo({
+      json: data,
+      radius: 2,
+    });
+    globe.add(countries);
+  });
+
+/**
+ * Sizes
+ */
+const sizes = {
+  width: window.innerWidth,
+  height: window.innerHeight,
+};
+
+window.addEventListener("resize", () => {
+  // Update sizes
+  sizes.width = window.innerWidth;
+  sizes.height = window.innerHeight;
+
+  // Update camera
+  camera.aspect = sizes.width / sizes.height;
+  camera.updateProjectionMatrix();
+
+  // Update renderer
+  renderer.setSize(sizes.width, sizes.height);
+  renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2));
+});
+
+/**
+ * Camera
+ */
+// Base camera
+const camera = new THREE.PerspectiveCamera(
+  75,
+  sizes.width / sizes.height,
+  0.1,
+  100
+);
+camera.position.x = 3;
+camera.position.y = 3;
+camera.position.z = 3;
+scene.add(camera);
+
+// Controls
+const controls = new OrbitControls(camera, canvas);
+controls.enableDamping = true;
+
+/**
+ * Renderer
+ */
+const renderer = new THREE.WebGLRenderer({
+  canvas: canvas,
+});
+renderer.setSize(sizes.width, sizes.height);
+renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2));
+
+/**
+ * Animate
+ */
+const clock = new THREE.Clock();
+
+const tick = () => {
+  const elapsedTime = clock.getElapsedTime();
+
+  globe.rotation.y = elapsedTime * 0.2
+
+  // Update controls
+  controls.update();
+
+  // Render
+  renderer.render(scene, camera);
+
+  // Call tick again on the next frame
+  window.requestAnimationFrame(tick);
+};
+
+tick();
